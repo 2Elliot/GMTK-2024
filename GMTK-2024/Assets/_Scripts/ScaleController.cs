4c4b0863dd92ffbd1ff29ae740059359fc5558f1
Elliot <93895160+2Elliot@users.noreply.github.com>
2024-08-16 18:54:22 -0700

    Physics

--- GMTK-2024/Assets/_Scripts/ScaleController.cs
+++ GMTK-2024/Assets/_Scripts/ScaleController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScaleController : MonoBehaviour {
@@ -11,34 +9,39 @@ public class ScaleController : MonoBehaviour {
         public Transform connectionPoint;
         public WeightController weightObject;
         public float weight;
-        [Range(-0.5f, 0.5f)] public float xPos;
+        [Range(1f, 4.5f)] public float xPos;
+        [Range(-1, 1)] public int direction; // -1 for left, 1 for right
     }
 
     [SerializeField] private Weight[] _weights;
     
     [SerializeField, Range(-30, 30)] private float _currentRotation = 0;
-    [SerializeField] private float _rotationMultiplier = 20f;
+    [SerializeField] private float _rotationMultiplier = 5f;
     
-    public float CalculateRotation(Weight weight1, Weight weight2) {
-        // Calculate the moments of each weight around the center (moment = weight * distance)
-        float moment1 = weight1.weight * weight1.xPos;
-        float moment2 = weight2.weight * weight2.xPos;
+    private float CalculateRotation(Weight weight1, Weight weight2) {
+        float moment1 = weight1.weight * weight1.xPos * weight1.direction;
+        float moment2 = weight2.weight * weight2.xPos * weight2.direction;
 
-        // Calculate the net moment
         float netMoment = moment1 + moment2;
 
-        // Convert net moment to an angle
         float angle = netMoment * _rotationMultiplier;
         angle = Mathf.Clamp(angle, -30f, 30f);
 
         return -angle; // Negative because Unity's weird
     }
-    
+
+    private void Start() {
+        for (int i = 0; i < _weights.Length; i++) {
+            _weights[i].weightObject.Index = i;
+            _weights[i].weightObject.scaleController = this;
+        }
+    }
+
     private void Update() {
 
         foreach (Weight currWeight in _weights) {
             Vector3 connectionPointLocalPosition = currWeight.connectionPoint.localPosition;
-            currWeight.connectionPoint.localPosition = new Vector3(currWeight.xPos, connectionPointLocalPosition.y,
+            currWeight.connectionPoint.localPosition = new Vector3(currWeight.xPos * currWeight.direction, connectionPointLocalPosition.y,
                 connectionPointLocalPosition.z);
 
             currWeight.weightObject.SetPosition(currWeight.connectionPoint.position);
@@ -50,8 +53,26 @@ public class ScaleController : MonoBehaviour {
             _currentRotation);
     }
 
+    public void SetWeightXPosition(int index, Vector2 position) {
+        Vector2 snappedPosition = SnapPointToLine(position);
+        Vector3 newPosition = transform.InverseTransformPoint(snappedPosition);
+        int direction = _weights[index].direction;
+        _weights[index].xPos = Mathf.Clamp(direction * newPosition.x, 1f, 4.5f); // jank af
+    }
 
+    private Vector2 SnapPointToLine(Vector2 point) {
+        Vector2 lineDirection = AngleToDirection(_currentRotation);
+        lineDirection.Normalize();
 
+        Vector2 pointToLineStart = point - (Vector2)transform.position;
+        float projectionLength = Vector2.Dot(pointToLineStart, lineDirection);
+        Vector2 projection = (Vector2)transform.position + projectionLength * lineDirection;
+        
+        return new Vector2(point.x, projection.y);
+    }
 
-
+    private static Vector2 AngleToDirection(float angle) {
+        float radians = Mathf.Deg2Rad * angle;
+        return new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
+    }
 }
--- GMTK-2024/Assets/_Scripts/WeightController.cs
+++ GMTK-2024/Assets/_Scripts/WeightController.cs
@@ -1,18 +1,57 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using InputHandler;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class WeightController : MonoBehaviour {
+    
+    private PlayerInputActions _inputActions;
+    private Camera _mainCamera;
+    
     [SerializeField] private Transform _connectionPoint;
 
     private Vector3 offset;
+
+    private bool _spriteClicked;
+
+    public int Index;
+    public ScaleController scaleController;
     
     private void Start() {
+        _inputActions = InputReader.Instance.InputActions;
+
+        _inputActions.Player.Mouse0.performed += OnClick;
+        _inputActions.Player.Mouse0.canceled += _ => { _spriteClicked = false; };
+
+        _mainCamera = Camera.main;
+        
         offset = transform.position - _connectionPoint.position;
     }
 
     public void SetPosition(Vector3 position) {
         transform.position = position + offset;
     }
+    
+    private void OnClick(InputAction.CallbackContext context) {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Vector2 worldPosition = _mainCamera.ScreenToWorldPoint(mousePosition);
+        
+        RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+        
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject == gameObject) {
+                _spriteClicked = true;
+            }
+        }
+    }
+
+    private void Update() {
+        if (_spriteClicked) {
+            Vector2 mousePositionScreen = Mouse.current.position.ReadValue();
+            Vector3 mousePositionWorld = _mainCamera.ScreenToWorldPoint(new Vector3(mousePositionScreen.x, mousePositionScreen.y, 0));
+        
+            scaleController.SetWeightXPosition(Index, mousePositionWorld);
+        }
+    }
+
 }
