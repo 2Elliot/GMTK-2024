3e2bb6df9d73b205d8809508006b5005637c9e63
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2024-08-19 23:04:54 -0700

    STORE DONE
    also enabled play reload

--- GMTK-2024/Assets/_Scripts/UI/ShopSlotGroupUI.cs
+++ GMTK-2024/Assets/_Scripts/UI/ShopSlotGroupUI.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 
 public class ShopSlotGroupUI : MonoBehaviour
 {
+    [SerializeField] private GameController _gameController;
     [SerializeField] private ShopManager _shopManager;
     [SerializeField] private GameObject _slotPrefab;
     [SerializeField] private List<ShopSlotPrefab> _slots;
 
-    private void Start() {
+    private void OnEnable() {
         _shopManager = SingletonContainer.Instance.ShopManager;
+        _gameController = SingletonContainer.Instance.GameController;
     }
 
     public void SetItems(List<PurchasableItem> items) {
         RemoveAllSlots();
+        print($"{items.Count}");
         for (int i = 0; i < items.Count; i++) {
             GameObject slot = Instantiate(_slotPrefab, transform);
             ShopSlotPrefab slotScript = slot.GetComponent<ShopSlotPrefab>();
             slotScript.SetItem(items[i]);
             slotScript.SetSelectButtonListener(OnSelectItemFromSlot);
+            print($"Null: _gameController {_gameController == null} items[i] {items[i] == null}");
+            if (!_gameController.CanAfford(items[i].Price)) {
+                print($"Cannot afford {items[i].ItemName}");
+                slotScript.DisableButton();
+            }
             _slots.Add(slotScript);
         }
     }
